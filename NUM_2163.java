2155d9f95ba75bcd0ec403853a6a18585c1acddf
myoungjinseo <smjsih@naver.com>
2021-08-18 20:22:10 +0900

    08-18

---
+++ NUM_2163.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class NUM_2163 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int result =0;
+		result = N*M-1;		//°á°ú °ª
+		System.out.println(result);
+	}
+
+}
---
+++ NUM_9316.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class NUM_9316 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T =sc.nextInt();
+		
+		for(int i=0; i<T; i++) {
+			System.out.println("Hello World, Judge "+(i+1)+"!");
+		}
+
+	}
+
+}
